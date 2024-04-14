3c6f1c9bf9e8dfa25e30bc904873bc431f5bda06
David O. Kuda <david@kuda.ai>
2024-04-14 22:28:39 +0200

    add PathValue / wildcard route

--- main.go
+++ main.go
@@ -4,6 +4,18 @@ import (
 	"log"
 	"net/http"
 )
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/songbook", listSongs)
+	mux.HandleFunc("/songbook/{song}", showSong)
+	mux.HandleFunc("/songbook/add", addSong)
+
+	log.Print("Starting web server, listening on port 8873")
+
+	err := http.ListenAndServe(":8873", mux)
+	log.Fatal(err)
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from kudaai"))
@@ -13,18 +25,12 @@ func listSongs(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display all songs"))
 }
 
-func createSong(w http.ResponseWriter, r *http.Request) {
+func showSong(w http.ResponseWriter, r *http.Request) {
+	song := r.PathValue("song")
+	w.Write([]byte("Requested Song " + song))
+}
+
+func addSong(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form to create a new song"))
 }
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/songs/view", listSongs)
-	mux.HandleFunc("/songs/create", createSong)
-
-	log.Print("Starting web server, listening on port 8873")
-
-	err := http.ListenAndServe(":8873", mux)
-	log.Fatal(err)
-}
