c544ea4221ed5b8192640c602d72c0ad7efe86df
David Kuda <davidkuda3@gmail.com>
2024-12-02 00:09:35 +0100

    refactor: create function for simple markdown pages

--- cmd/web/handlers_simple_pages.go
+++ cmd/web/handlers_simple_pages.go
@@ -16,6 +16,13 @@ type Page struct {
 	CurrentPath string
 }
 
+func newPageFromMarkdown(markdown []byte, r *http.Request) *Page {
+	htmlBytes := blackfriday.Run(markdown)
+	content := template.HTML(htmlBytes)
+	pageData := newPage(content, r)
+	return &pageData
+}
+
 func newPage(content template.HTML, r *http.Request) Page {
 	return Page{
 		Title:       getTitleFromRequestPath(r),
@@ -71,80 +78,60 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func getPageAbout(w http.ResponseWriter, r *http.Request) {
 	md, err := os.ReadFile("./data/pages/about.md")
-
 	if err != nil {
 		log.Printf("Error reading file: %s", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
-	htmlBytes := blackfriday.Run(md)
-	content := template.HTML(htmlBytes)
-	pageData := newPage(content, r)
-
-	getSimplePage(w, &pageData)
+	pageData := newPageFromMarkdown(md, r)
+	getSimplePage(w, pageData)
 }
 
 func getPageBlog(w http.ResponseWriter, r *http.Request) {
 	md, err := os.ReadFile("./data/pages/blog.md")
-
 	if err != nil {
 		log.Printf("Error reading file: %s", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
-	htmlBytes := blackfriday.Run(md)
-	content := template.HTML(htmlBytes)
-	pageData := newPage(content, r)
-
-	getSimplePage(w, &pageData)
+	pageData := newPageFromMarkdown(md, r)
+	getSimplePage(w, pageData)
 }
 
 func getPageBookshelf(w http.ResponseWriter, r *http.Request) {
 	md, err := os.ReadFile("./data/pages/bookshelf.md")
-
 	if err != nil {
 		log.Printf("Error reading file: %s", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
-	htmlBytes := blackfriday.Run(md)
-	content := template.HTML(htmlBytes)
-	pageData := newPage(content, r)
-
-	getSimplePage(w, &pageData)
+	pageData := newPageFromMarkdown(md, r)
+	getSimplePage(w, pageData)
 }
 
 func getPageCV(w http.ResponseWriter, r *http.Request) {
 	// TODO: wouldn't it be nice to generate a beautiful PDF from this site?
 	md, err := os.ReadFile("./data/pages/CV.md")
-
 	if err != nil {
 		log.Printf("Error reading file: %s", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
-	htmlBytes := blackfriday.Run(md)
-	content := template.HTML(htmlBytes)
-	pageData := newPage(content, r)
+	pageData := newPageFromMarkdown(md, r)
 	pageData.Title = "CV"
-
-	getSimplePage(w, &pageData)
+	getSimplePage(w, pageData)
 }
 
 func getPageTIL(w http.ResponseWriter, r *http.Request) {
 	md, err := os.ReadFile("./data/pages/til.md")
-
 	if err != nil {
 		log.Printf("Error reading file: %s", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
-	htmlBytes := blackfriday.Run(md)
-	content := template.HTML(htmlBytes)
-	pageData := newPage(content, r)
+	pageData := newPageFromMarkdown(md, r)
 	pageData.Title = "Today I Learned"
-
-	getSimplePage(w, &pageData)
+	getSimplePage(w, pageData)
 }
 
 func getSimplePage(w http.ResponseWriter, p *Page) {
