089d1b30e153f830c6124837a01a43371dcfbd56
David Kuda <davidkuda3@gmail.com>
2025-07-19 23:45:12 +0200

    remove many css lines by using data-theme for color theme switching

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,1 +1,36 @@
 console.log("Hello from the go server")
+
+// ------------------------------------------------------------
+// DOM references
+// ------------------------------------------------------------
+const themeBtn = document.getElementById("themeToggle");
+
+
+// ------------------------------------------------------------
+// event listeners
+// ------------------------------------------------------------
+themeBtn.addEventListener("click", async () => {
+	const cur = document.documentElement.getAttribute("data-theme") || "dark";
+	const next = cur === "dark" ? "light" : "dark";
+	applyTheme(next);
+	await localStorage.setItem("theme", next);
+});
+
+
+// ------------------------------------------------------------
+// functions
+// ------------------------------------------------------------
+function applyTheme(mode) {
+	document.documentElement.setAttribute("data-theme", mode);
+	themeBtn.textContent = mode === "dark" ? "light mode" : "dark mode";
+}
+
+
+// ------------------------------------------------------------
+// init
+// ------------------------------------------------------------
+(async () => {
+	const theme = await localStorage.getItem("theme");
+	applyTheme(theme);
+})();
+
